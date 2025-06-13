ba9ebe37caee37dbbc323e93f23e206bd80fac90
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2025-06-13 10:06:56 +0000

    He creado una variable donde almaceno un string, que me da una informacion importante

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+
+  let prueba = "Prueba para verificar los cambios"
   //write your code here
   console.log("Hello Rigo from the console!");
 };
